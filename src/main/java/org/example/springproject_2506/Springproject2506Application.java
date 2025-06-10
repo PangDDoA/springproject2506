e4ebf748c481f895abf495e02daf5517936462e7
eogus623 <eogus623@gmail.com>
2025-06-10 15:48:10 +0900

    made project

---
+++ src/main/java/org/example/springproject_2506/Springproject2506Application.java
@@ -0,0 +1,13 @@
+package org.example.springproject_2506;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springproject2506Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springproject2506Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springproject_2506/Springproject2506ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springproject_2506;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproject2506ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
