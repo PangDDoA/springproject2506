f9f6fe44bc6ce49af0c37c1e503b5c8e0a3743bb
eogus623 <eogus623@gmail.com>
2025-06-13 15:59:28 +0900

    create repository, service(user)

--- src/main/java/org/example/springproject_2506/repository/UserRepository.java
+++ src/main/java/org/example/springproject_2506/repository/UserRepository.java
@@ -4,4 +4,4 @@ import org.example.springproject_2506.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-}
+} // JpaRepository 를 상속받으면서 자동으로 CRDU가 작성됐기 떄문에 해당 코드는 다 작성함.
